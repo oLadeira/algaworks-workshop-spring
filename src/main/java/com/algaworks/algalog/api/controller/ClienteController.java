174f1561aa47653cc976d1bdf6c784aad80d2452
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-10-26 14:23:56 -0300

    configuring Jakarta Persistence to list Cliente and configuring Cliente controller

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,6 +2,10 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algalog.domain.model.Cliente;
@@ -9,22 +13,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController //dizendo ao Spring que esta classe é um controlador 
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
+	
 	@GetMapping("/clientes") //mapeamento, quando a requisição "/clientes" for solicitada, esta função será executada
 	public List<Cliente> listar() {
-		Cliente c1 = new Cliente();
-		Cliente c2 = new Cliente();
-		
-		c1.setId(1L);
-		c1.setNome("João");
-		c1.setTelefone("11922833");
-		c1.setEmail("joaodascolves@algaworks.com");
-		
-		c2.setId(2L);
-		c2.setNome("Maria");
-		c2.setTelefone("11933853");
-		c2.setEmail("mariasilva@algaworks.com");
-		
-		return Arrays.asList(c1, c2);		
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 	
 }
